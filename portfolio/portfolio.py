c40bbeecd5de5e635a24f74363121d0d1b90b25a
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-11 13:01:25 +0100

    Update portfolio.py

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,1 +1,23 @@
-
+portfolio = [
+    {"symbol": "BTC", "quantity": 0.00043331},
+    {"symbol": "BTC.b", "quantity": 0.0113417},
+    {"symbol": "ETH", "quantity": 0.22375339},
+    {"symbol": "WETH", "quantity": 0.0002109},
+    {"symbol": "WETH.e", "quantity": 0.111998},
+    {"symbol": "wstETH", "quantity": 0.0187779},
+    {"symbol": "DOT", "quantity": 252.0325},
+    {"symbol": "KSM", "quantity": 2.19},
+    {"symbol": "AVAX", "quantity": 12.9},
+    {"symbol": "WAVAX", "quantity": 0.0024956},
+    {"symbol": "sAVAX", "quantity": 3.82},
+    {"symbol": "LINK", "quantity": 22},
+    {"symbol": "LINK.e", "quantity": 18.9},
+    {"symbol": "GRT", "quantity": 1366.1},
+    {"symbol": "CAPS", "quantity": 19217.1121},
+    {"symbol": "SOL", "quantity": 0.07633722},
+    {"symbol": "TIA", "quantity": 1.7},
+    {"symbol": "OMI", "quantity": 94000},
+    {"symbol": "USDT", "quantity": 0.53771299},
+    {"symbol": "USDC", "quantity": 0},
+    {"symbol": "DAI", "quantity": 0}
+]
