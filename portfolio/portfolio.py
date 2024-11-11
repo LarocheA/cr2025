a1bb0b89ce65d8361435d374067a430f17372a56
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-11 10:36:29 +0100

    Update portfolio.py

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,1 +1,24 @@
+from .transaction import Transaction
 
+class Portfolio:
+    def __init__(self):
+        self.holdings = {}
+        self.transactions = []
+
+    def add_transaction(self, crypto_id, amount, price, date):
+        transaction = Transaction(crypto_id, amount, price, date)
+        self.transactions.append(transaction)
+        if crypto_id in self.holdings:
+            self.holdings[crypto_id] += amount
+        else:
+            self.holdings[crypto_id] = amount
+
+    def get_holdings(self):
+        return self.holdings
+
+    def get_total_value(self, crypto_data):
+        total = 0
+        for crypto_id, amount in self.holdings.items():
+            price = crypto_data.get_price(crypto_id)
+            total += amount * price
+        return total
