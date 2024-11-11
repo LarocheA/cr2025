c23fc9f47b0a95ff8b5f5c6ce453337f3270cfca
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-11 14:51:22 +0100

    Update api_client.py

--- data/api_client.py
+++ data/api_client.py
@@ -1,14 +1,12 @@
-# data/api_client.py
-
 import requests
-from config import API_KEY, BASE_URL, FEAR_GREED_URL
+from config import API_KEY_CRYPTOCOMPARE, API_KEY_COINMARKETCAP, BASE_URL_CRYPTOCOMPARE, BASE_URL_COINMARKETCAP, FEAR_GREED_URL
 
 def get_crypto_price(symbol, vs_currency='USD'):
-    url = f"{BASE_URL}/price"
+    url = f"{BASE_URL_CRYPTOCOMPARE}/price"
     params = {
         "fsym": symbol,
         "tsyms": vs_currency,
-        "api_key": API_KEY
+        "api_key": API_KEY_CRYPTOCOMPARE
     }
     response = requests.get(url, params=params)
     if response.status_code == 200:
@@ -17,12 +15,12 @@ def get_crypto_price(symbol, vs_currency='USD'):
         raise Exception(f"Error fetching price for {symbol}: {response.status_code}")
 
 def get_historical_data(symbol, vs_currency='USD', limit=2000):
-    url = f"{BASE_URL}/histoday"
+    url = f"{BASE_URL_CRYPTOCOMPARE}/histoday"
     params = {
         "fsym": symbol,
         "tsym": vs_currency,
         "limit": limit,
-        "api_key": API_KEY
+        "api_key": API_KEY_CRYPTOCOMPARE
     }
     response = requests.get(url, params=params)
     if response.status_code == 200:
@@ -33,7 +31,8 @@ def get_historical_data(symbol, vs_currency='USD', limit=2000):
 def get_fear_and_greed_index():
     response = requests.get(FEAR_GREED_URL)
     if response.status_code == 200:
-        data = response.json()
-        return int(data['data'][0]['value'])
-    else:
-        raise Exception(f"Error fetching Fear and Greed Index: {response.status_code}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        fng_value = soup.find('div', class_='fng-value')
+        if fng_value:
+            return int(''.join(filter(str.isdigit, fng_value.text)))
+    return None
