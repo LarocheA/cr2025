6e95646d42dff88f0353aad908debd2bf9b63307
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-11 11:41:31 +0100

    Update transaction.py

--- portfolio/transaction.py
+++ portfolio/transaction.py
@@ -1,11 +1,1 @@
-from datetime import datetime
 
-class Transaction:
-    def __init__(self, crypto_id, amount, price, date=None):
-        self.crypto_id = crypto_id
-        self.amount = amount
-        self.price = price
-        self.date = date or datetime.now()
-
-    def __str__(self):
-        return f"Transaction: {self.crypto_id}, Amount: {self.amount}, Price: {self.price}, Date: {self.date}"
