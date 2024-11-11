df4cebe4209bacdacde40b9b3bf1b9696b3923c7
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-11 13:31:30 +0100

    Update crypto_data.py

--- data/crypto_data.py
+++ data/crypto_data.py
@@ -1,1 +1,8 @@
+# data/crypto_data.py
 
+from .api_client import get_crypto_price
+
+def update_portfolio_prices(portfolio):
+    for crypto in portfolio:
+        crypto['price'] = get_crypto_price(crypto['symbol'])
+    return portfolio
