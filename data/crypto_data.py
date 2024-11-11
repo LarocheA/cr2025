d32cf16a311d56239a077efdbbc2083553f6693c
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-11 14:07:21 +0100

    Update crypto_data.py

--- data/crypto_data.py
+++ data/crypto_data.py
@@ -1,8 +1,15 @@
 # data/crypto_data.py
 
-from .api_client import get_crypto_price
+import pandas as pd
+from .api_client import get_crypto_price, get_historical_data
 
 def update_portfolio_prices(portfolio):
     for crypto in portfolio:
         crypto['price'] = get_crypto_price(crypto['symbol'])
     return portfolio
+
+def get_historical_dataframe(portfolio):
+    historical_data = {}
+    for crypto in portfolio:
+        historical_data[crypto['symbol']] = get_historical_data(crypto['symbol'])
+    return pd.DataFrame(historical_data)
