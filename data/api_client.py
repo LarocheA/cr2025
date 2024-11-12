966d73b7268216c8db0ed52ddb5aa69ffbb9dc61
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-12 13:03:00 +0100

    Update api_client.py

--- data/api_client.py
+++ data/api_client.py
@@ -1,26 +1,34 @@
 import requests
+from requests.exceptions import RequestException
+from bs4 import BeautifulSoup
 from config import API_KEY_CRYPTOCOMPARE, API_KEY_COINMARKETCAP, BASE_URL_CRYPTOCOMPARE, BASE_URL_COINMARKETCAP, FEAR_GREED_URL
 
 def get_crypto_price(symbol, vs_currency='USD'):
+    """
+    Récupère le prix actuel d'une cryptomonnaie.
+    
+    :param symbol: Le symbole de la cryptomonnaie
+    :param vs_currency: La devise de référence (par défaut USD)
+    :return: Le prix actuel de la cryptomonnaie
+    """
     url = f"{BASE_URL_CRYPTOCOMPARE}/price"
     params = {
         "fsym": symbol,
         "tsyms": vs_currency,
         "api_key": API_KEY_CRYPTOCOMPARE
     }
-    response = requests.get(url, params=params)
-    
-    # Vérifiez si la requête a réussi
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
         data = response.json()
-        # Vérifiez si 'Data' et 'USD' existent dans la réponse
-        if 'Data' in data and vs_currency in data['Data']:
-            return data['Data'][vs_currency]
+        if vs_currency in data:
+            return data[vs_currency]
         else:
             raise KeyError(f"Les données pour {symbol} en {vs_currency} ne sont pas disponibles.")
-    else:
-        raise Exception(f"Erreur lors de la récupération du prix pour {symbol}: {response.status_code}")
+    except RequestException as e:
+        raise Exception(f"Erreur lors de la récupération du prix pour {symbol}: {str(e)}")
 
+# 
 def get_historical_data(symbol, vs_currency='USD', limit=2000):
     url = f"{BASE_URL_CRYPTOCOMPARE}/histoday"
     params = {
