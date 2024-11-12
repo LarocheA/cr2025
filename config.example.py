d045fbd61a1b51f67e7d5f76bdfb4c708967e7b1
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-12 13:15:23 +0100

    Add files via upload

---
+++ config.example.py
@@ -0,0 +1,13 @@
+# config.example.py
+
+API_KEY_CRYPTOCOMPARE = "YOUR_CRYPTOCOMPARE_API_KEY"
+API_KEY_COINMARKETCAP = "YOUR_COINMARKETCAP_API_KEY"
+BASE_URL_CRYPTOCOMPARE = "https://min-api.cryptocompare.com/data/v2"
+BASE_URL_COINMARKETCAP = "https://pro-api.coinmarketcap.com/v2"
+FEAR_GREED_URL = "https://alternative.me/crypto/fear-and-greed-index/"
+
+PORTFOLIO = [
+    {"symbol": "BTC", "quantity": 0.01177501},
+    {"symbol": "ETH", "quantity": 0.22375339},
+    # Ajoutez ici le reste de votre portefeuille
+]
