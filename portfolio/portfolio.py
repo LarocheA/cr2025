e7402fa26b48140490eb6975293c90c7608cd4e4
Laroche <120012584+LarocheA@users.noreply.github.com>
2024-11-12 13:56:34 +0100

    Update portfolio.py

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -32,10 +32,10 @@ class Portfolio:
 
     def add_crypto(self, symbol, quantity):
         if symbol in self.df['symbol'].values:
-        self.df.loc[self.df['symbol'] == symbol, 'quantity'] += quantity
+            self.df.loc[self.df['symbol'] == symbol, 'quantity'] += quantity
         else:
-        new_crypto = {'symbol': symbol, 'quantity': quantity}
-        self.df = self.df.append(new_crypto, ignore_index=True)
+            new_crypto = {'symbol': symbol, 'quantity': quantity}
+            self.df = self.df.append(new_crypto, ignore_index=True)
         self.update_values()
 
 def update_values(self):
